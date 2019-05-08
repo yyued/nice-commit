a324ee5f27322434531c824f96628087104ca4b3
lijialiang <lijialiang@yy.com>
2019-05-08 18:12:36 +0800

    feat: eslintignore

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const fs = require('fs')
 
 const projectPath = path.resolve(process.cwd(), '../../')
 const eslintConfig = path.resolve(projectPath, '.eslintrc.js')
+const eslintIgnoreConfig = path.resolve(projectPath, '.eslintignore')
 const huskyConfig = path.resolve(projectPath, '.huskyrc')
 
 if (fs.existsSync(eslintConfig)) {
   console.error('[error: eslint conf exist]')
 } else {
   fs.writeFileSync(eslintConfig, fs.readFileSync(path.resolve(__dirname, './eslint-config/eslintrc')))
+}
+
+if (fs.existsSync(eslintIgnoreConfig)) {
+  console.error('[error: eslint ignore conf exist]')
+} else {
+  fs.writeFileSync(eslintIgnoreConfig, fs.readFileSync(path.resolve(__dirname, './eslint-config/eslintignore')))
 }
 
 if (fs.existsSync(huskyConfig)) {
