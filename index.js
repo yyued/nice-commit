172a72d9a768924ae9130f57912757010df192b1
lijialiang <lijialiang@yy.com>
2019-07-22 18:18:26 +0800

    feat: choose type of eslint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs')
+const inquirer = require('inquirer')
 
 const projectPath = path.resolve(process.cwd(), '../../')
 const eslintConfig = path.resolve(projectPath, '.eslintrc.js')
@@ -9,42 +10,70 @@ const commitlintConfig = path.resolve(projectPath, '.commitlintrc.js')
 const packageJson = path.resolve(projectPath, 'package.json')
 const editorConfig = path.resolve(projectPath, '.editorconfig')
 
-const warn = msg => console.log(msg)
-
-if (!fs.existsSync(packageJson)) {
-  throw 'error: package.json not exist'
-}
-
-if (fs.existsSync(editorConfig)) {
-  warn('warn: editor conf exist')
-} else {
-  fs.writeFileSync(editorConfig, fs.readFileSync(path.resolve(__dirname, '.editorconfig')))
-}
-
-if (fs.existsSync(eslintConfig)) {
-  warn('warn: eslint conf exist');
-} else {
-  fs.writeFileSync(eslintConfig, fs.readFileSync(path.resolve(__dirname, 'eslint-config/eslintrc')))
-}
-
-if (fs.existsSync(eslintIgnoreConfig)) {
-  warn('warn: eslint ignore conf exist')
-} else {
-  fs.writeFileSync(eslintIgnoreConfig, fs.readFileSync(path.resolve(__dirname, 'eslint-config/eslintignore')))
-}
-
-if (fs.existsSync(huskyConfig)) {
-  warn('warn: husky conf exist')
-} else {
-  fs.writeFileSync(huskyConfig, JSON.stringify({
-    "hooks": {
-      "commit-msg": "eslint --ext .vue,.js ./ && commitlint -e $GIT_PARAMS"
+;(async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'eslintType',
+      message: 'eslint 类型',
+      choices: [
+        { name: 'JS', value: '' },
+        { name: 'TS', value: '/ts' },
+        { name: 'Vue.js', value: '/vue' },
+        { name: 'Vue.ts', value: '/vue-ts' }
+      ]
     }
-  }, null, 4))
-}
+  ])
 
-if (fs.existsSync(commitlintConfig)) {
-  warn('warn: commitlint conf exist')
-} else {
-  fs.writeFileSync(commitlintConfig, fs.readFileSync(path.resolve(__dirname, 'commitlint.config.js')))
+  const warn = msg => console.log(msg)
+
+  if (!fs.existsSync(packageJson)) {
+  // eslint-disable-next-line no-throw-literal
+    throw 'error: package.json not exist'
+  }
+
+  if (fs.existsSync(editorConfig)) {
+    warn('warn: editor conf exist')
+  } else {
+    fs.writeFileSync(editorConfig, fs.readFileSync(path.resolve(__dirname, '.editorconfig')))
+  }
+
+  if (fs.existsSync(eslintConfig)) {
+    warn('warn: eslint conf exist')
+  } else {
+    const eslintConfigString =
+`module.exports = {
+  extends: [
+    'yyued${answers.eslintType}'
+  ]
 }
+`
+    fs.writeFileSync(eslintConfig, eslintConfigString)
+  }
+
+  if (fs.existsSync(eslintIgnoreConfig)) {
+    warn('warn: eslint ignore conf exist')
+  } else {
+    const eslintIgnoreConfigString =
+`dist
+assets
+`
+    fs.writeFileSync(eslintIgnoreConfig, eslintIgnoreConfigString)
+  }
+
+  if (fs.existsSync(huskyConfig)) {
+    warn('warn: husky conf exist')
+  } else {
+    fs.writeFileSync(huskyConfig, JSON.stringify({
+      'hooks': {
+        'commit-msg': 'eslint --ext .vue,.js ./ && commitlint -e $GIT_PARAMS'
+      }
+    }, null, 4))
+  }
+
+  if (fs.existsSync(commitlintConfig)) {
+    warn('warn: commitlint conf exist')
+  } else {
+    fs.writeFileSync(commitlintConfig, fs.readFileSync(path.resolve(__dirname, 'commitlint.config.js')))
+  }
+})()
